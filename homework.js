7c5a09e4924d7ff6d81456922f485f8f963c5e3e
Diego Alonso Ojeda Medina <daom89@gmail.com>
2016-11-07 12:50:32 -0500

    Finish my homework

--- homework.js
+++ homework.js
@@ -9,3 +9,19 @@ if (APPLE_QUANTITY < 5){
   console.log("You have 5 or more apples. Hurrah!");
 }
 
+// 2. Write a "for" loop
+for(i = 0; i < APPLE_QUANTITY; i ++){
+  console.log("WEEEEEEE! I'm looping!")
+}
+
+// 3. Write a function
+var sayHello = function(){
+  console.log("Hello!")
+}
+
+// 4. Create an object
+var mary = {
+  name: "Mary Rose",
+  codeNinja: true,
+  country: "UK"
+}
