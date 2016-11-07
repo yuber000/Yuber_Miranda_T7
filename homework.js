23bf9667a28e4193341171bc3a34a6cb1e304164
Diego Alonso Ojeda Medina <daom89@gmail.com>
2016-11-07 12:50:02 -0500

    Write if/else statement

---
+++ homework.js
@@ -0,0 +1,11 @@
+// Mary Rose's JavaScript Homework
+
+// 1. Write an if/else statement
+var APPLE_QUANTITY = 5;
+
+if (APPLE_QUANTITY < 5){
+  console.log("You have fewer than 5 apples.");
+} else {
+  console.log("You have 5 or more apples. Hurrah!");
+}
+
